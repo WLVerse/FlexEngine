d1dc34bdb8112ed687eb527cf1154c646b51cb92
Wen Loong <wenloong@portylstudios.com>
2024-12-11 15:17:27 +0800

    oops flipped the display order

--- Editor/src/Layers/statisticspanellayer.cpp
+++ Editor/src/Layers/statisticspanellayer.cpp
@@ -40,21 +40,21 @@ namespace Editor
       {
         auto& layerstack = window->GetLayerStack();
 
-        ImGui::Text("Layer Count: %d", layerstack.GetLayerCount());
         ImGui::Text("Overlay Count: %d", layerstack.GetOverlayCount());
+        ImGui::Text("Layer Count: %d", layerstack.GetLayerCount());
 
         ImGui::Separator();
 
         ImGui::Text("Layer Stack");
         size_t index = 0;
-        for (size_t i = 0; i < layerstack.GetLayerCount(); ++i, ++index)
-        {
-          ImGui::Text("  [%d] %s", index, layerstack.GetLayerName(i).c_str());
-        }
-        ImGui::Separator();
         for (size_t i = 0; i < layerstack.GetOverlayCount(); ++i, ++index)
         {
           ImGui::Text("  [%d] %s", index, layerstack.GetOverlayName(i).c_str());
+        }
+        ImGui::Separator();
+        for (size_t i = 0; i < layerstack.GetLayerCount(); ++i, ++index)
+        {
+          ImGui::Text("  [%d] %s", index, layerstack.GetLayerName(i).c_str());
         }
       }
 
