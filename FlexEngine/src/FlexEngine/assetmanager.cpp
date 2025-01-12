a074a2d82cec68e430411aa2da55e9da5fe51ea7
Drakainagi <42331211+Drakainagi@users.noreply.github.com>
2025-01-12 13:39:12 +0800

    <Update> Logged Fonts on Console with assetManager

--- FlexEngine/src/FlexEngine/assetmanager.cpp
+++ FlexEngine/src/FlexEngine/assetmanager.cpp
@@ -153,6 +153,14 @@ namespace FlexEngine
           assets[key] = Asset::Sound{ key }; // create sound asserts on FMOD side and shouldn't need here
           FLX_FLOW_ENDSCOPE();
         }
+        else if (file_extension.string() == ".ttf")
+        {
+            FLX_FLOW_BEGINSCOPE();
+            AssetKey key = file.path.string().substr(default_directory_length);
+            assets[key] = Asset::Font{ key };
+            FLX_FLOW_ENDSCOPE();
+            Log::Info("Loaded font path: " + key);
+        }
       }
     );
 
