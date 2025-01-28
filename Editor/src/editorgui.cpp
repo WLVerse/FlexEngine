616627ddf1c045cdf0d370ae74d5523885b3941c
Mr Rock <44529454+FeelsFeels@users.noreply.github.com>
2025-01-29 05:04:26 +0800

    Entity name fix, Mat4x4 * vec4

--- Editor/src/editorgui.cpp
+++ Editor/src/editorgui.cpp
@@ -130,8 +130,7 @@ namespace Editor
 		std::string entity_name{ "(no entity)" };
 		if (entity != FlexECS::Entity::Null)
 		{
-			//entity_name = FLX_STRING_GET(*entity.GetComponent<FlexEngine::FlexECS::Scene::StringIndex>());
-      entity_name = entity_name.substr(0, entity_name.find_first_of('\0'));
+			entity_name = FLX_STRING_GET(*entity.GetComponent<EntityName>());
 		}
 
 		ImGui::BeginGroup();
@@ -143,6 +142,7 @@ namespace Editor
 
 		if (auto payload = StartPayloadReceiver<FlexECS::EntityID>(PayloadTags::ENTITY))
 		{
+			//Warning: needs a way to check that the dropped entity is not itself.
 			FlexECS::Entity dropped_entity = *payload;
 			entity = dropped_entity;
 			EndPayloadReceiver();
