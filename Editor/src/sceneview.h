fa28eb506a6fbc5fb6ffbbf0d306e94a4333a562
Mr Rock <44529454+FeelsFeels@users.noreply.github.com>
2025-02-16 03:33:25 +0800

    drag multiselect

--- Editor/src/sceneview.h
+++ Editor/src/sceneview.h
@@ -14,11 +14,28 @@
 #pragma once
 #include "editorpanel.h"
 
+/******************************
+		SCENE KEYBOARD CONTROLS
+*Only works when Scene is in focus
+
+Camera controls:
+>	 Middle mouse button / LAlt + Rightclick - Pan camera
+>	 Scroll zoomy zoom
+
+Entity Manipulation:
+>	 W E R - Gizmos, Translate Scale Rotate respectively
+>	 LCtrl + click on entity - multiselect
+>	 Delete key - delete
+
+Debug / visualizers:
+>	 Shift + T - toggle camera bounds visualizer
+*******************************/
+
+
 namespace Editor
 {
 	class SceneView : public EditorPanel
 	{
-		#ifndef GAME
 	public:
 		void Init();
 		void Update();
@@ -36,14 +53,21 @@ namespace Editor
 		void CalculateViewportPosition();	//position of viewport image and size
 		void HandleMouseAndKeyboardEvents();
 		FlexEngine::FlexECS::Entity FindClickedEntity();
+		void DraggingEntitySelect();
+		void FindEntitiesInSelection();
+
 		FlexEngine::Vector4 GetWorldClickPosition();
+		FlexEngine::Vector4 ScreenToWorld(ImVec2);
 		ImVec2 WorldToScreen(const FlexEngine::Vector2& position);
 		void DrawGizmos();
 		void MoveEditorCam();
 		void DisplayMainCamBounds();
 
 		bool m_gizmo_hovered;
-		bool m_dragging;
+		bool m_dragging_gizmo;
+
+		ImVec2 m_drag_selection_start_point;
+		bool m_dragging_selection { false };
 
 		bool m_dragging_camera{ false };
 		bool m_display_cam_bounds { true };
@@ -60,6 +84,6 @@ namespace Editor
 
 		FlexEngine::FlexECS::Entity m_editor_camera;
 		FlexEngine::Vector3 m_camera_position{ 0,0,0 };
-		#endif
+
 	};
 }
