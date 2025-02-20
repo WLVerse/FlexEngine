ee0318718b7f08bd81c0bfe8354512a0bb793d9e
DireVision <yewchong25@live.com>
2025-02-20 18:00:08 +0800

    mem leak code, editor&game no console appearing

--- FlexEngine/src/entrypoint.h
+++ FlexEngine/src/entrypoint.h
@@ -41,6 +41,7 @@ int main(int, char**)
   // Enable run-time memory check for debug builds.
   #ifdef _DEBUG
   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+  //int* leak = new int;
   #endif
 
   // Ensure that the FreeQueue is run at the end of the program.
